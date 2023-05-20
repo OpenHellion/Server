0769721fcfea7b91d2781bc3ed054fce9338df69
Makkkkus <37084190+Makkkkus@users.noreply.github.com>
2023-05-20 21:08:35 +0200

    Simulation: Use dotnet timer

--- ZeroGravity/Objects/SolarSystem.cs
+++ ZeroGravity/Objects/SolarSystem.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using OpenHellion.Networking;
+using OpenHellion;
 using ZeroGravity.Data;
 using ZeroGravity.Math;
 using ZeroGravity.Network;

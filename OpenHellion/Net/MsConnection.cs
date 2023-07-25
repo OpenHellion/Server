c95a123548e1798fa67fa5b153c81670ae5403cc
Makkkkus <37084190+Makkkkus@users.noreply.github.com>
2023-07-26 00:28:35 +0200

    Networking -> Net

--- OpenHellion/Net/MsConnection.cs
+++ OpenHellion/Net/MsConnection.cs
@@ -19,8 +19,6 @@
 using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Net.Http.Json;
-using System.Net.WebSockets;
 using System.Text;
 using System.Threading.Tasks;
 using OpenHellion.Exceptions;
